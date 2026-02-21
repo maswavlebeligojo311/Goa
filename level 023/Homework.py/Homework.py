a741788667cf869e2c12c524c12be65823d53e54
maswavlebeligojo311 <nikolozgelashvili101@gmail.com>
2026-02-21 12:39:48 -0500

    commit

--- level 023/Homework.py/Homework.py
+++ level 023/Homework.py/Homework.py
@@ -9,3 +9,34 @@ elif Grade >= 60 and Grade < 70:
     print ("D")
 else:
     print ("F")
+
+
+
+
+number = int(input("აირჩიე რიცხვი:"))
+
+if number > 0:
+    print("რიცხვი დადებითია")
+elif number < 0:
+    print("რიცხვი უარყოფითია")
+else:
+    print("რიცხვი არის ნული")
+
+
+Num1 = int(input("enter first number:"))
+Num2 = int(input("enter second number:"))
+if Num1 > Num2:
+    print("First Number is Greater than second one")
+else:
+    print("Second Number is Greater than first one")
+
+
+
+temperature = int(input("ცელსიუსი:"))
+
+if temperature < 0:
+    print("Cold")
+elif 0 >= temperature and temperature <= 30:
+    print("Normal")
+else:
+    print("Hot")
