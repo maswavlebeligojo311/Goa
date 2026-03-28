f9f6dae8d105e8b30823d94f5f47c35f076455ac
maswavlebeligojo311 <nikolozgelashvili101@gmail.com>
2026-03-28 10:16:22 -0400

    commit

---
+++ Level 033/Homework/py.py
@@ -0,0 +1,26 @@
+
+
+sentence = input("write a sentence:")
+print(sentence.upper())
+print(sentence.lower())
+
+
+movie = input("enter you favourite movie:")
+print(movie.capitalize())
+
+a1 = input("enter a word: ")
+a2 = input("enter a letter: ")
+print(a1.find(a2))
+
+
+
+something = input("idk:")
+print(something.swapcase())
+
+
+
+#lower ხდის ასოებსს პატარასს
+#upper ხდის ასოუებს დიდს
+#swapcase ცვლის დიდ და პატარა ასოებს ერთმანეთში
+#capitalze სიტყვის პირველ ასოს ხდის დიდს
+
---
+++ Level 035/Classwork/py.py
@@ -0,0 +1,19 @@
+list1 = ["something", "idk", "sun", "moon", "sky", "earth", "moon",]
+list1.append(True)
+list1.append(False)
+print(len(list1))
+print (list1)
+
+list1.insert(2, 5)
+list1.insert(4, 10)
+print (list1)
+
+
+list2 = ["Footbal", "Basketball", "Tennis", "Voleyball", "Swimming"]
+list2.pop(2)
+list2.remove("Basketball")
+print (list2)
+
+list3 = ["idk", 20.5, 50, False, "nothing"]
+print(len(list3))
+print (list3)
