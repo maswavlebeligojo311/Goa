4b82e884f3e97493fceee5594249fcd6ff1f20ee
maswavlebeligojo311 <nikolozgelashvili101@gmail.com>
2026-06-11 18:12:42 -0400

    commit

---
+++ Level 052/Classwork/py.py
@@ -0,0 +1,42 @@
+#1)https://www.codewars.com/kata/545afd0761aa4c3055001386/train/python
+
+# def take(arr,n):
+#     return arr[:n]
+
+
+
+#2)https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/python
+
+
+# def include(arr, item):
+#     if item in arr:
+#         return True
+#     else:
+#         return False
+
+
+#3)https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python
+
+
+# def sum_two_smallest_numbers(numbers):
+#     numbers = sorted(numbers)
+#     return numbers[0] + numbers[1]
+
+
+# #4)https://www.codewars.com/kata/55b42574ff091733d900002f/train/python
+
+# def friend(x):
+#     friends = []
+#     for i in x:
+#         if len(i) == 4:
+#             friends.append(i)
+#     return friends
+
+# #5)https://www.codewars.com/kata/5949481f86420f59480000e7/train/python
+
+
+# def odd_or_even(arr):
+#     if sum(arr) % 2 == 0:
+#         return "even"
+#     else:
+#         return "odd"
---
+++ Level 052/Homework/py.py
@@ -0,0 +1,30 @@
+#1)https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/python
+
+# def solution(text, ending):
+#     return text.endswith(ending)
+
+
+#2)https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/python
+
+# def longest(a1, a2):
+#     return "".join(sorted(a1 + a2))
+
+#??????????????????????????????
+
+
+# #3)https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/python
+# def longest(a1, a2):
+#     concatenation = a1 + a2
+#     st = set(concatenation)
+#     return "".join(sorted(st))
+
+
+# #4)https://www.codewars.com/kata/5865918c6b569962950002a1/train/python
+
+
+# def str_count(strng, letter):
+#     numbers = 0
+#     for idk in strng:
+#         if idk == letter:
+#             numbers += 1
+#     return numbers
