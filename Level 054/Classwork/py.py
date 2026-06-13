431c793a2e4f3562f2ac203db7476f958bdbd024
maswavlebeligojo311 <nikolozgelashvili101@gmail.com>
2026-06-13 13:52:11 -0400

    commit

---
+++ Level 054/Classwork/py.py
@@ -0,0 +1,17 @@
+#1)https://www.codewars.com/kata/56541980fa08ab47a0000040/train/python
+
+# def printer_error(s):
+#     string = "abcdefghijklm"
+#     count = 0
+#     for i in s:
+#         if i not in string:
+#             count += 1
+#     return f"{count}/{len(s)}"
+
+
+#2)https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/python
+
+# def gimme(input_array):
+#     a = sorted(input_array)
+#     b = a[1]
+#     return input_array.index(b)
---
+++ Level 054/Homework/py.py
@@ -0,0 +1,28 @@
+#1)https://www.codewars.com/kata/57a429e253ba3381850000fb/train/python
+
+# def bmi(weight, height):
+#     idk = weight / (height * height)
+#     if idk <= 18.5:
+#         return "Underweight"
+#     elif idk <= 25.0:
+#         return "Normal"
+#     elif idk <= 30.0:
+#         return "Overweight"
+#     elif idk > 30:
+#         return "Obese"
+
+
+
+#2)https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/python
+
+# # write the function is_anagram
+# def is_anagram(test, original):
+#     if sorted(test.upper()) == sorted(original.upper()):
+#         return True
+#     else:
+#         return False
+
+#3)https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/python
+
+# def remove_url_anchor(url):
+#     return url.split("#")[0]
